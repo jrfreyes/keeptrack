af5e7c60ea444524bdb4aa9e41347c346af348a1
jrfreyes <justinrupert.reyes@gmail.com>
2022-05-06 01:01:27 +0900

    Complete Lab 8

---
+++ src/projects/ProjectCard.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import { Project } from './Project';
+
+function formatDescription(description) {
+    return description.substring(0, 60) + '...';
+}
+
+export default function ProjectCard({ project }) {
+    return (
+        <div class="card">
+            <img src={project.imageUrl} alt={project.name} />
+            <section class="section dark">
+                <h5 class="strong">
+                    <strong>{project.name}</strong>
+                </h5>
+                <p>{formatDescription(project.description)}</p>
+                <p>Budget : {project.budget.toLocaleString()}</p>
+            </section>
+        </div>
+    );
+}
+
+ProjectCard.propTypes = {
+    project: PropTypes.instanceOf(Project).isRequired
+};
--- src/projects/ProjectsList.js
+++ src/projects/ProjectsList.js
@@ -1,37 +1,18 @@
 import PropTypes from 'prop-types'
 import { Project } from './Project';
+import ProjectCard from './ProjectCard';
 
 export default function ProjectsList({ projects }) {
-    return (
-        <div className='row'>
-            {projects.map(project => (
-                <ProjectCard key={project.id} project={project}/>
-            ))}
+    const items = projects.map(project => (   
+        <div key={project.id} class="cols-sm">
+            <ProjectCard project={project}/>
         </div>
-    );
+    ))
+    
+    return <div className='row'>{items}</div>;
 }
 
 ProjectsList.propTypes = {
     projects: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired
 }
 
-function ProjectCard({project}) {
-    return (
-        <div class="cols-sm">
-            <div class="card">
-                <img src={project.imageUrl} alt={project.name} />
-                <section class="section dark">
-                <h5 class="strong">
-                    <strong>{project.name}</strong>
-                </h5>
-                <p>{project.description}</p>
-                <p>Budget : {project.budget.toLocaleString()}</p>
-                </section>
-            </div>
-        </div>
-    )
-}
-
-ProjectCard.propTypes = {
-    project: PropTypes.instanceOf(Project).isRequired
-}
