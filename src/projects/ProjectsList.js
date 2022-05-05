633fd3f67c8a34c7ec8a85ece2ccb331edd1f4c7
jrfreyes <justinrupert.reyes@gmail.com>
2022-05-06 01:13:50 +0900

    Complete Lab 10

--- src/projects/ProjectsList.js
+++ src/projects/ProjectsList.js
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types'
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
+import ProjectForm from './ProjectForm';
 
 export default function ProjectsList({ projects }) {
     const items = projects.map(project => (   
         <div key={project.id} class="cols-sm">
-            <ProjectCard project={project}/>
+            <ProjectCard project={project} />
+            <ProjectForm project={project} />
         </div>
     ))
     
