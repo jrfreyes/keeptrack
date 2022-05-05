a744c4d02da1f3bdb378a41ae53412bac2cb6a17
jrfreyes <justinrupert.reyes@gmail.com>
2022-05-06 00:54:23 +0900

    Complete Lab 7

--- src/projects/ProjectsList.js
+++ src/projects/ProjectsList.js
@@ -2,9 +2,36 @@ import PropTypes from 'prop-types'
 import { Project } from './Project';
 
 export default function ProjectsList({ projects }) {
-    return <pre>{JSON.stringify(projects, null, ' ')}</pre>;
+    return (
+        <div className='row'>
+            {projects.map(project => (
+                <ProjectCard key={project.id} project={project}/>
+            ))}
+        </div>
+    );
 }
 
 ProjectsList.propTypes = {
     projects: PropTypes.arrayOf(PropTypes.instanceOf(Project)).isRequired
+}
+
+function ProjectCard({project}) {
+    return (
+        <div class="cols-sm">
+            <div class="card">
+                <img src={project.imageUrl} alt={project.name} />
+                <section class="section dark">
+                <h5 class="strong">
+                    <strong>{project.name}</strong>
+                </h5>
+                <p>{project.description}</p>
+                <p>Budget : {project.budget.toLocaleString()}</p>
+                </section>
+            </div>
+        </div>
+    )
+}
+
+ProjectCard.propTypes = {
+    project: PropTypes.instanceOf(Project).isRequired
 }
