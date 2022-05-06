ff70a9766fc6778bd3d729f13dbe1bdc03f79ed8
jrfreyes <justinrupert.reyes@gmail.com>
2022-05-06 20:45:15 +0900

    Complete Lab 20

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './home/HomePage';
 import ProjectsPage from './projects/ProjectsPage';
+import { BrowserRouter as Router, NavLink, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='container'>
-      <ProjectsPage />
-    </div>
+    <Router>
+      <header className='sticky'>
+        <span className='logo'>
+          <img src='/assets/logo-3.svg' alt='logo' width='49' height='99' />
+        </span>
+        <NavLink to='/' className='button rounded'>
+          <span className='icon-home' />
+          Home
+        </NavLink>
+        <NavLink to="/projects" className='button rounded'>
+          Projects
+        </NavLink>
+      </header>
+      <div className='container'>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
